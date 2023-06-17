810764e4fcbdac55e366770ca505af96ce415b3b
Ilija Spasic <spasicilija1@gmail.com>
2023-06-17 21:44:07 +0200

    Start front end implementation. Add jQuery package
    Create folder structure, and add jQuery scripts.
    Start implementation of the landing page with simple
    product list display.

---
+++ UserInterface/JS/index.js
@@ -0,0 +1,37 @@
+﻿let products;
+
+$(document).ready(() => {
+    $.ajax({
+        url: "http://localhost:60471/api/products/all",
+        type: "GET",
+        dataType: "json",
+        success: function (response) {
+            console.log(response);
+            products = response;
+            $.each(products, function (index, product) {
+                // Create the HTML elements for each product
+                var div = $("<div></div>").addClass("product");;
+                var title = $("<p></p>").text(product.Title);
+                var image = $("<img>").attr("src", product.Image);
+                var price = $("<p></p>").text(product.Price + "$");
+
+                // Append the elements to the container div
+                div.append(title, image, price);
+                $(".items-container").append(div);
+            });
+
+        },
+        error: function (xhr, status, error) {
+
+            // Handle any errors that occur during the AJAX request
+            console.error(error);
+        }
+    });
+
+
+});
+
+
+function ProductDetails(index) {
+
+}
